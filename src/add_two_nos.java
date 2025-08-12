76a4fc1d2692a91e3fc564374989068b757c2331
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2025-08-12 23:50:10 +0530

    Fix in code.

--- src/add_two_nos.java
+++ src/add_two_nos.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class add_two_nos {
-    public static void main (String args[]){
+    public static void main (String[] args){
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter first number=");
         int a= sc.nextInt();
