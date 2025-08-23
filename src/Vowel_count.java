245d93394889c748859c022bd4059cd367d48c59
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2025-08-23 21:55:21 +0530

    Modify code for better understanding.

--- src/Vowel_count.java
+++ src/Vowel_count.java
@@ -10,7 +10,8 @@ public class Vowel_count {
 
         for(int i=0; i<str.length(); i++){
             char ch=str.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                    ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
                 count++;
             }
         }
