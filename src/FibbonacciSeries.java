b27ce84e677b60a728a83068d075c6082776a9e8
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2025-08-29 23:04:54 +0530

    Program to print the fibonacci series up to n terms.

---
+++ src/FibbonacciSeries.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FibbonacciSeries {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms= ");
+        int n = sc.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.println("Fibbonacci series");
+        for (int i =1; i<=n; i++){
+            System.out.println(first + " ");
+            int next= first + second;
+            first = second;
+            second = next;
+        }
+
+    }
+}
